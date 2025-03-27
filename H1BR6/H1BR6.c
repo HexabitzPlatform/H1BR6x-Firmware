b982bb52702fec1fea8e0b7b28976b33f81a376c
Mahmood_Reda <mr4141705@gmail.com>
2025-03-27 17:40:47 +0300

    add ((GetModuleParameter)) empty function

--- H1BR6/H1BR6.c
+++ H1BR6/H1BR6.c
@@ -39,10 +39,7 @@ extern FLASH_ProcessTypeDef pFlash;
 extern uint8_t numOfRecordedSnippets;
 
 /* Module exported parameters ------------------------------------------------*/
-
-
-
-module_param_t modParam[NUM_MODULE_PARAMS] ={{.paramPtr = NULL, .paramFormat =FMT_FLOAT, .paramName =""}};
+module_param_t modParam[NUM_MODULE_PARAMS] ={0};
 
 /* Private variables ---------------------------------------------------------*/
 const uint8_t numberMap[3] = {1, 10, 100};
@@ -566,6 +563,28 @@ void Module_Peripheral_Init(void){
 		xTaskCreate(LogTask, (const char *) "LogTask", (2*configMINIMAL_STACK_SIZE), NULL, osPriorityNormal-osPriorityIdle, &LogTaskHandle);
 }
 
+/***************************************************************************/
+/* This functions is useful only for input (sensors) modules.
+ * @brief: Samples a module parameter value based on parameter index.
+ * @param paramIndex: Index of the parameter (1-based index).
+ * @param value: Pointer to store the sampled float value.
+ * @retval: Module_Status indicating success or failure.
+ */
+Module_Status GetModuleParameter(uint8_t paramIndex,float *value){
+	Module_Status status =BOS_OK;
+
+	switch(paramIndex){
+
+		/* Invalid parameter index */
+		default:
+			status =BOS_ERR_WrongParam;
+			break;
+	}
+
+	return status;
+}
+
+
 /*-----------------------------------------------------------*/
 /* --- H0BR4 message processing task.
  */
@@ -770,32 +789,32 @@ void LogTask(void * argument)
 												case CLOSED:	strcat(lineBuffer, "CLOSED"); break;
 												case CLICKED:	strcat(lineBuffer, "CLICKED"); break;
 												case DBL_CLICKED:	strcat(lineBuffer, "DBL_CLICKED"); break;
-												case PRESSED:	strcat(lineBuffer, "PRESSED"); break;
-												case RELEASED:	strcat(lineBuffer, "RELEASED"); break;
-												case PRESSED_FOR_X1_SEC:
-													if (button[logVars[i].source].pressedX1Sec)
-														sprintf((char *)lineBuffer, "%sPRESSED_FOR_%d_SEC", (char *) lineBuffer, button[logVars[i].source].pressedX1Sec);
-													break;
-												case PRESSED_FOR_X2_SEC:
-													if (button[logVars[i].source].pressedX2Sec)
-														sprintf((char *)lineBuffer, "%sPRESSED_FOR_%d_SEC", (char *) lineBuffer, button[logVars[i].source].pressedX2Sec);
-													break;
-												case PRESSED_FOR_X3_SEC:
-													if (button[logVars[i].source].pressedX3Sec)
-														sprintf((char *)lineBuffer, "%sPRESSED_FOR_%d_SEC", (char *) lineBuffer, button[logVars[i].source].pressedX3Sec);
-													break;
-												case RELEASED_FOR_Y1_SEC:
-													if (button[logVars[i].source].releasedY1Sec)
-														sprintf((char *)lineBuffer, "%sRELEASED_FOR_%d_SEC", (char *) lineBuffer, button[logVars[i].source].releasedY1Sec);
-													break;
-												case RELEASED_FOR_Y2_SEC:
-													if (button[logVars[i].source].releasedY2Sec)
-														sprintf((char *)lineBuffer, "%sRELEASED_FOR_%d_SEC", (char *) lineBuffer, button[logVars[i].source].releasedY2Sec);
-													break;
-												case RELEASED_FOR_Y3_SEC:
-													if (button[logVars[i].source].releasedY3Sec)
-														sprintf((char *)lineBuffer, "%sRELEASED_FOR_%d_SEC", (char *) lineBuffer, button[logVars[i].source].releasedY3Sec);
-													break;
+//												case PRESSED:	strcat(lineBuffer, "PRESSED"); break;
+//												case RELEASED:	strcat(lineBuffer, "RELEASED"); break;
+//												case PRESSED_FOR_X1_SEC:
+//													if (button[logVars[i].source].pressedX1Sec)
+//														sprintf((char *)lineBuffer, "%sPRESSED_FOR_%d_SEC", (char *) lineBuffer, button[logVars[i].source].pressedX1Sec);
+//													break;
+//												case PRESSED_FOR_X2_SEC:
+//													if (button[logVars[i].source].pressedX2Sec)
+//														sprintf((char *)lineBuffer, "%sPRESSED_FOR_%d_SEC", (char *) lineBuffer, button[logVars[i].source].pressedX2Sec);
+//													break;
+//												case PRESSED_FOR_X3_SEC:
+//													if (button[logVars[i].source].pressedX3Sec)
+//														sprintf((char *)lineBuffer, "%sPRESSED_FOR_%d_SEC", (char *) lineBuffer, button[logVars[i].source].pressedX3Sec);
+//													break;
+//												case RELEASED_FOR_Y1_SEC:
+//													if (button[logVars[i].source].releasedY1Sec)
+//														sprintf((char *)lineBuffer, "%sRELEASED_FOR_%d_SEC", (char *) lineBuffer, button[logVars[i].source].releasedY1Sec);
+//													break;
+//												case RELEASED_FOR_Y2_SEC:
+//													if (button[logVars[i].source].releasedY2Sec)
+//														sprintf((char *)lineBuffer, "%sRELEASED_FOR_%d_SEC", (char *) lineBuffer, button[logVars[i].source].releasedY2Sec);
+//													break;
+//												case RELEASED_FOR_Y3_SEC:
+//													if (button[logVars[i].source].releasedY3Sec)
+//														sprintf((char *)lineBuffer, "%sRELEASED_FOR_%d_SEC", (char *) lineBuffer, button[logVars[i].source].releasedY3Sec);
+//													break;
 												case NONE:
 													if (logs[j].type == RATE)
 													{
