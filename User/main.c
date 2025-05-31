0ef860d1a03b7a6060f5569c3b0b95eb5e2d4b18
Mahmood_Reda <mr4141705@gmail.com>
2025-05-31 16:19:53 +0300

    clear topology & main files .

--- User/main.c
+++ User/main.c
@@ -1,89 +1,38 @@
 /*
- BitzOS (BOS) V0.2.9 - Copyright (C) 2017-2023 Hexabitz
+ BitzOS (BOS) V0.4.0 - Copyright (C) 2017-2025 Hexabitz
  All rights reserved
 
  File Name     : main.c
  Description   : Main program body.
  */
-/* Includes ------------------------------------------------------------------*/
+
+/* Includes ****************************************************************/
 #include "BOS.h"
-uint32_t counter00 = 0;
-/* Private variables ---------------------------------------------------------*/
 
-/* Private function prototypes -----------------------------------------------*/
+/* Private variables *******************************************************/
 
-//void buttonClickedCallback(uint8_t port);
-//void buttonPressedForXCallback(uint8_t port, uint8_t eventType);
-/* Main function ------------------------------------------------------------*/
+/* Private Function Prototypes *********************************************/
 
+/* Main Function ***********************************************************/
 int main(void){
 
-	Module_Init();		//Initialize Module &  BitzOS
+	/* Initialize Module &  BitzOS */
+	Module_Init();
 
-
-
-	//Don't place your code here.
-	for(;;){}
+	/* Don't place your code here */
+	for(;;){
+	}
 }
 
-/*-----------------------------------------------------------*/
-
+/***************************************************************************/
 /* User Task */
 void UserTask(void *argument){
 
-
-//	AddPortButton(MOMENTARY_NO, 3);								// Define a button connected to port P1
-//		SetButtonEvents(3, 1, 0, 3, 0, 0, 0, 0, 0,1);		// Activate a click event and a pressed_for_x event for 3 seconds
-//
-//		// Create log and log button clicks
-//		//if ( CreateLog("Click Logger", EVENT, 10, FMT_TAB, FMT_TIME, "Sample @ 10Hz") == H1BR6_OK )
-//		if ( CreateLog("Click Logger", EVENT, 10, FMT_TAB, FMT_SAMPLE, "Sample @ 10Hz") == H1BR6_OK )
-//		{
-//			LogVar("Click Logger", PORT_BUTTON, P3, "Logger");
-//			LogVar("Click Logger", MEMORY_DATA_UINT32, (uint32_t)&counter00, "counter00");
-//			// Do not reset button state after writing the log since we need it to blink LED as well!
-//			needToDelayButtonStateReset = true;
-//		}
-	//SD_getSpace();
-    //SD_writeString("file1.txt","Hello");
-	//void SD_writeVariable(char* FileName,uint8_t Variable);
-	//void SD_read();
-	//void SD_readFile(char* FileName);
-	//void SD_fileUpdate();
-  //  SD_removeFile("file2.TXT");
-  // SD_unmount();
-	// put your code here, to run repeatedly.
+	/* put your code here, to run repeatedly. */
 	while(1){
-	//	buttonClickedCallback(3);
-		//buttonPressedForXCallback(3, MOMENTARY_NO);
+
 	}
 }
-void buttonClickedCallback(uint8_t port)
-{
-	++counter00;
 
-	if (counter00 == 1)	StartLog("Click Logger");
-
-	if ( counter00%10 == 0 ) {
-		IND_blink(1000);
-	} else {
-		IND_blink(200);
-	}
-
-	NeedToDelayButtonStateReset = false;		// Reset button state now
-}
-
-void buttonPressedForXCallback(uint8_t port, uint8_t eventType)
-{
-	// The first PressedForX event we defined in SetButtonEvents was called
-//	if (eventType == 1)
-//	{
-//		StopLog("Click Logger");
-//		SetButtonEvents(P3, 0, 0, 0, 0, 0, 0, 0, 0,1);
-//		IND_blink(400); Delay_ms(400); IND_blink(400);
-//	}
-
-	NeedToDelayButtonStateReset = false;// Reset button state now
-	counter00=0;
-}
-/*-----------------------------------------------------------*/
+/***************************************************************************/
+/***************** (C) COPYRIGHT HEXABITZ ***** END OF FILE ****************/
--- User/topology.h
+++ User/topology.h
@@ -1,20 +1,19 @@
 /*
- BitzOS (BOS) V0.3.6 - Copyright (C) 2017-2024 Hexabitz
+ BitzOS (BOS) V0.4.0 - Copyright (C) 2017-2025 Hexabitz
  All rights reserved
 
  File Name     : topology.h
  Description   : Array topology definition.
-
  */
 
-/* Define to prevent recursive inclusion -------------------------------------*/
+/* Define to prevent recursive inclusion ***********************************/
 #ifndef __topology_H
 #define __topology_H
 #ifdef __cplusplus
  extern "C" {
 #endif
 
-/* Includes ------------------------------------------------------------------*/
+/* Includes ****************************************************************/
 #include "stm32g0xx_hal.h"
 
 #define __N	2					// Number of array modules
@@ -24,14 +23,14 @@
 #define _mod2	2<<3
 
 // Topology
-static uint16_t array[__N ][7] ={
-	{_H1BR6, _mod2 | P1, 0, 0, 0, 0, 0}, 				     				 // Module 1
-	{_H01R0, _mod1 | P1, 0, 0, 0, 0, 0},					    			 // Module 2
+static uint16_t array[__N][7] = {
+      	{_H1BR6, _mod2 | P1, 0, 0, 0, 0, 0}, 								 // Module 1
+        {_H01R0, _mod1 | P1, 0, 0, 0, 0, 0},					    		 // Module 2
 };
 
 // Configurations for duplex serial ports
 #if ( _module == 1 )
-	#define	H1BR6	        1
+	#define	H1BR6	1
 	#define	_P1pol_normal	1
 	#define	_P2pol_normal	1
 	#define	_P3pol_normal	1
@@ -48,13 +47,12 @@ static uint16_t array[__N ][7] ={
 	#define	_P4pol_normal	1
 	#define	_P5pol_normal	1
 	#define	_P6pol_normal	1
-
 #endif
-
 
 #ifdef __cplusplus
 }
 #endif
 #endif /*__ topology_H */
 
-/************************ (C) COPYRIGHT HEXABITZ *****END OF FILE****/
+/***************************************************************************/
+/***************** (C) COPYRIGHT HEXABITZ ***** END OF FILE ****************/
